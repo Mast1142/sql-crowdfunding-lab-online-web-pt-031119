3b517944aa3b19806fddc95cd0cd6e04d9c6acf4
Samuel T <ssamt1142@gmail.com>
2019-05-26 16:22:11 +0000

    Automatically backed up by Learn

---
+++ lib/create.sql
@@ -0,0 +1,5 @@
+CREATE TABLE projects (title TEXT, category TEXT, funding_goal INTEGER, start_date TEXT, end_date TEXT)
+
+CREATE TABLE user(name TEXT, age INTEGER)
+
+CREATE TABLE pledge(amount INTEGER, user_id INTEGER, project_id INTEGER)
